f50ee88022caf1f27ea8d550b686ef3350e781cd
unknown <ziparaujo@gmail.com>
2024-07-05 11:05:35 -0300

    criação projeto spring

---
+++ src/main/java/com/generation/joana/JoanaApplication.java
@@ -0,0 +1,13 @@
+package com.generation.joana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoanaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoanaApplication.class, args);
+	}
+
+}
