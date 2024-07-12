9969597138447afd54408eb3ba034ff056369387
unknown <ziparaujo@gmail.com>
2024-07-12 12:04:39 -0300

    :sparkles: feat: Finalização do CRUD de produto

--- src/main/java/com/generation/joana/controller/ProdutoController.java
+++ src/main/java/com/generation/joana/controller/ProdutoController.java
@@ -1,17 +1,28 @@
 package com.generation.joana.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.generation.joana.model.Produto;
+import com.generation.joana.repository.CategoriaRepository;
 import com.generation.joana.repository.ProdutoRepository;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/produtos")
@@ -20,6 +31,9 @@ public class ProdutoController {
 
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	@GetMapping
 	public ResponseEntity<List<Produto>> getAll() {
@@ -31,6 +45,41 @@ public class ProdutoController {
 		return produtoRepository.findById(id)
 				.map(resposta -> ResponseEntity.ok(resposta))
 				.orElse(ResponseEntity.notFound().build());
+	}
+	
+	@GetMapping("/nome/{nome}")
+	public ResponseEntity<List<Produto>> getByNome(@PathVariable String nome) {
+		return ResponseEntity.ok(produtoRepository.getAllByNomeContainingIgnoreCase(nome));
+	}
+	
+	@PostMapping
+	public ResponseEntity<Produto> post(@Valid @RequestBody Produto produto) {
+		if(categoriaRepository.existsById(produto.getCategoria().getId()))
+			return ResponseEntity.status(HttpStatus.CREATED)
+					.body(produtoRepository.save(produto));
+		throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Categoria inexistente.", null);
+	}
+	
+	@PutMapping
+	public ResponseEntity<Produto> put(@Valid @RequestBody Produto produto) {
+		if(categoriaRepository.existsById(produto.getId())) {
+			if(categoriaRepository.existsById(produto.getCategoria().getId()))
+					return ResponseEntity.status(HttpStatus.OK)
+							.body(produtoRepository.save(produto));
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Categoria inexistente.", null);
+		}
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+	}
+	
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable Long id) {
+		Optional<Produto> produto = produtoRepository.findById(id);
+		
+		if(produto.isEmpty())
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+		
+		produtoRepository.deleteById(id);
 	}
 
 }
