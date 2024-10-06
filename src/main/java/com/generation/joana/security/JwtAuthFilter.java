ecd34b7fd940806b4fc6499e19d5648ed1319fa7
beazinat <beatrizgarodrigues@gmail.com>
2024-10-05 23:13:16 -0300

    :recycle: Refactor: Refatoração na camada Spring Security

--- src/main/java/com/generation/joana/security/JwtAuthFilter.java
+++ src/main/java/com/generation/joana/security/JwtAuthFilter.java
@@ -31,7 +31,7 @@ public class JwtAuthFilter extends OncePerRequestFilter {
     private UserDetailsServiceImpl userDetailsService;
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal(@SuppressWarnings("null") HttpServletRequest request, @SuppressWarnings("null") HttpServletResponse response, @SuppressWarnings("null") FilterChain filterChain) throws ServletException, IOException {
         String authHeader = request.getHeader("Authorization");
         String token = null;
         String username = null;
--- src/main/java/com/generation/joana/security/UserDetailsImpl.java
+++ src/main/java/com/generation/joana/security/UserDetailsImpl.java
@@ -2,7 +2,6 @@ package com.generation.joana.security;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
 
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
